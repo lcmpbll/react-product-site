443b64cd1297066602f8fb52cd8fe6d1e684d7a5
Liam Cammpbell <liam22campbell@gmail.com>
2023-05-06 13:37:54 -0700

    Add brands section and css

--- src/components/brand/index.jsx
+++ src/components/brand/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './index.css';
+import { google, slack, shopify, atlassian, dropbox } from './imports'; 
 
 const Brand = () => {
   return (
-    <div>brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt='google'/>
+      </div>
+      <div>
+        <img src={slack} alt='slack'/>
+      </div>
+      <div>
+        <img src={atlassian} alt='Atlassian'/>
+      </div>
+      <div>
+        <img src={shopify} alt='shopify'/>
+      </div>
+      <div>
+        <img src={dropbox} alt='dropbox'/>
+      </div>
+    </div>
   );
 }
 
