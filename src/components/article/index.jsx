0471c24eeefbc257ca67a5d96107b75cef5d42c6
Liam Cammpbell <liam22campbell@gmail.com>
2023-05-06 15:55:33 -0700

    Add Article css and component

--- src/components/article/index.jsx
+++ src/components/article/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './index.css'
 
-const Article = () => {
+const Article = ({imgUrl, imgAlt, date, title}) => {
   return (
-    <div>article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={imgUrl} alt={imgAlt}/>
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
