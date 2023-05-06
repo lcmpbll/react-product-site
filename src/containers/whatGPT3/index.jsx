b3b8d261ec29a5f5277564e2fd3a3994a763cdda
Liam Cammpbell <liam22campbell@gmail.com>
2023-05-06 14:02:40 -0700

    Add whatgpt3 section and styles.

--- src/containers/whatGPT3/index.jsx
+++ src/containers/whatGPT3/index.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import Feature from '../../components/feature'
 import './index.css'
+
+const featuresArray = [
+  {
+    title: "What is GPT-3",
+    text: "We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+  },
+  {
+    title: "Chatbots",
+    text: "We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+  }, 
+  {
+    title: "Knowledgebase",
+    text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"
+  }, 
+  {
+    title: "Education",
+    text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+  }
+]
 
 const WhatGPT = () => {
   return (
-    <div>
-      Blog
+    <div className='gpt3__whatgpt3 section__margin' id='whatgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination.</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
