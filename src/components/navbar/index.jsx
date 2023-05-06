7d56ba82ed318b32727aa16eab4869012cfc2b5d
Liam Cammpbell <liam22campbell@gmail.com>
2023-05-05 22:27:18 -0700

    Start navbar, import initial files

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './index.css';
+import logo from '../../assets/logo.svg';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 
 const  Navbar = () => {
   return (
-    <div></div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links-logo'>
+          <img src={logo} alt='logo'/>
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#whatGPT'>What is GPT?</a></p>
+          <p><a href='#possibility'>Open Ai</a></p>
+          <p><a href='#caseStudy'>Case Studies</a></p>
+          <p><a href='#library'>Libraries</a></p>
+        </div>
+      </div>
+    </div>
   );
 }
 
