e6a1fa7f2c454ac3f1415e675e20ef71e06a0aba
Liam Cammpbell <liam22campbell@gmail.com>
2023-05-06 14:30:32 -0700

    add feature component with styling

--- src/components/feature/index.jsx
+++ src/components/feature/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './index.css';
-const Feature = () => {
+const Feature = ({title, text}) => {
   return (
-    <div>feature</div>
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container__feature-title'>
+        <div/>
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container__feature-text'>
+        <p>{text}</p>
+      </div>
+    </div>
   );
 }
 
