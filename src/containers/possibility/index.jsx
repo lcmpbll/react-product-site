fbc7e2066643d517b00270f1504a248dc58f3d83
Liam Cammpbell <liam22campbell@gmail.com>
2023-05-06 15:11:06 -0700

    Add possibilities section and css

--- src/containers/possibility/index.jsx
+++ src/containers/possibility/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import possibilityImage from '../../assets/possibility.png';
 import './index.css'
 
 const Possibility = () => {
   return (
-    <div>
-      Blog
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt='possibility'/>
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access to get Started</h4>
+        <h1 className='gradient__text'>The possibilities are beyond our imagination</h1>
+        <p>AI has the potential to automate tedious tasks, personalize experiences, make predictions, assist in healthcare, education, entertainment, robotics, communication, and security, among other applications. As AI continues to advance, new possibilities and use cases are likely to emerge.</p>
+        <h4>Request Early Access to get Started</h4>
+      </div>
     </div>
   )
 }
